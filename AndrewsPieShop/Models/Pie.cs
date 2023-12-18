4452b84782177caf7bbb8600a249f1459ab0d18b
Andrew Virts <asvirts@outlook.com>
2023-12-17 18:52:21 -0600

    Creating models

--- AndrewsPieShop/Models/Pie.cs
+++ AndrewsPieShop/Models/Pie.cs
@@ -1,8 +1,20 @@
-﻿namespace AndrewsPieShop.Models
+﻿using System.ComponentModel;
+
+namespace AndrewsPieShop.Models
 {
     public class Pie
     {
         public int PieId { get; set; }
-        public TYPE Type { get; set;}
+        public string Name { get; set; } = string.Empty;
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string? AllergyInfo { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
     }
 }
