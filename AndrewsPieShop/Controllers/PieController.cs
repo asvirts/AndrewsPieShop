436b6ffc549fa3c555a25c58061452d89ce8019c
Andrew Virts <asvirts@outlook.com>
2023-12-17 20:15:14 -0600

    Adding controller

---
+++ AndrewsPieShop/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using AndrewsPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndrewsPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+
+    }
+}
